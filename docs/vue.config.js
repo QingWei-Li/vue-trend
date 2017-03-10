39a5e9502213fc021f780dedd46f53b2cb134e22
qingwei.li <cinwell.li@gmail.com>
2017-03-11 03:52:06 +0800

    add publicPath

--- docs/vue.config.js
+++ docs/vue.config.js
@@ -2,5 +2,10 @@ var resolve = require('path').resolve
 module.exports = {
   html: {
     template: resolve(__dirname, './template.tpl')
+  },
+  webpack: function (webpackConfig) {
+    webpackConfig.output.publicPath = '/vue-trend/'
+
+    return webpackConfig
   }
 }
